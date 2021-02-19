d15787d89fc73100cdd1ab47a48eb587fa7e5441
Kewin Souto Barros <kevinstbarros@gmail.com>
2021-02-19 15:18:51 -0300

    inicioprojeto

---
+++ doabook/src/main/java/com/hardtech/doabook/DoabookApplication.java
@@ -0,0 +1,13 @@
+package com.hardtech.doabook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoabookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoabookApplication.class, args);
+	}
+
+}
